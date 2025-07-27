1a84cb76303660bd766d9bb7692bc80e7b655e7b
KowshikSuggala25 <saikowshiksuggala9390@gmail.com>
2025-07-27 18:55:00 +0530

    Updated App.TSX

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
-import { Footer } from "@/components/layout/footer";
+import { Footer } from "./components/Layout/Footer";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Children from "./pages/Children";
